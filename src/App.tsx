964b1adca60bb0b7035d54595285b35d218165ac
Leonardo <leonardobazzobonato@gmail.com>
2022-01-11 20:59:14 -0300

    NewRoom screen and auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-
-import { Home } from './pages/Home';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { AuthContextProvider } from "./contexts/AuthContext";
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
 
 function App() {
   return (
-   <Home />
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="rooms/new" element={<NewRoom />} />
+        </Routes>
+      </AuthContextProvider>
+    </BrowserRouter>
   );
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,23 @@
+import { useNavigate } from "react-router-dom";
+import googleIconImg from "../assets/images/google-icon.svg";
 import illustrationImg from "../assets/images/illustration.svg";
 import logoImg from "../assets/images/logo.svg";
-import googleIconImg from "../assets/images/google-icon.svg";
-
-import '../styles/auth.scss'
 import { Button } from "../components/Button";
+import "../styles/auth.scss";
+import { useAuth } from "../hooks/useAuth";
 
 export function Home() {
+  const history = useNavigate();
+  const { user, signInWithGoogle } = useAuth()
+
+  async function handleCreateRoom() {
+    if (!user) {
+      await signInWithGoogle();
+    }
+
+    history("/rooms/new");
+  }
+
   return (
     <div id="page-auth">
       <aside>
@@ -14,13 +26,13 @@ export function Home() {
         <p>Tire as dúvidas da sua audiência em tempo real</p>
       </aside>
       <main>
-        <div className='main-content'>
+        <div className="main-content">
           <img src={logoImg} alt="" />
-          <button className='create-room'>
+          <button onClick={handleCreateRoom} className="create-room">
             <img src={googleIconImg} alt="" />
             Crie sua sala com o google
           </button>
-          <div className='separator'>ou entre em uma sala</div>
+          <div className="separator">ou entre em uma sala</div>
           <form>
             <input type="text" placeholder="Digite o código da sala" />
             <Button type="submit">Entrar na sala</Button>
